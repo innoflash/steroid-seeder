9aa17575d8866584043517893b3e6a217cf53277
innoflash <innocentmazando@gmail.com>
2020-06-25 22:08:21 +0200

    availed steroidFactory

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,24 @@
 <?php
+
+use Innoflash\SteroidSeeder\Factory;
+
+if (! function_exists('steroidFactory')) {
+    /**
+     * Create a model factory builder for a given class and amount.
+     *
+     * @param  string  $class
+     * @param  int  $amount
+     *
+     * @return \Innoflash\SteroidSeeder\FactoryBuilder
+     */
+    function steroidFactory($class, $amount = null)
+    {
+        $factory = app(Factory::class);
+
+        if (isset($amount) && is_int($amount)) {
+            return $factory->of($class)->times($amount);
+        }
+
+        return $factory->of($class);
+    }
+}
