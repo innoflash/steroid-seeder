6b60e337de70a73b03f1bfa1b5c329076eee5492
innoflash <innocentmazando@gmail.com>
2020-06-25 22:05:25 +0200

    preped factory

--- src/Factory.php
+++ src/Factory.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Factory as LaravelFactory;
 
 class Factory extends LaravelFactory
 {
+    /**
+     * Create a builder for the given model.
+     *
+     * @param  string  $class
+     *
+     * @return \Innoflash\SteroidSeeder\FactoryBuilder
+     */
+    public function of($class)
+    {
+        return new FactoryBuilder(
+            $class, $this->definitions, $this->states,
+            $this->afterMaking, $this->afterCreating, $this->faker
+        );
+    }
 
 }
